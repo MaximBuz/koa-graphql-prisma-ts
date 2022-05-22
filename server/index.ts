ee32cd3986f27a16111defe156440c59b980acb1
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-22 10:41:14 +0200

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from 'apollo-server-koa';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import Koa from 'koa';
+import cors from '@koa/cors'
+import http from 'http';
+
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  const httpServer = http.createServer();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  const app = new Koa();
+  app.use(cors());
+  server.applyMiddleware({ app });
+  httpServer.on('request', app.callback());
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
